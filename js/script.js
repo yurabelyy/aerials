1fb6b8d03d8f2d5f611cb86396e011ff75237f8e
yurabelyy <yura.belyy.2015@mail.ru>
2023-04-20 18:05:46 +0300

    create

---
+++ js/script.js
@@ -0,0 +1,7 @@
+const burger = document.querySelector('.header-burger');
+const headerMenu = document.querySelector('.header-menu-wrapper');
+
+burger.addEventListener('click', function () {
+  headerMenu.classList.toggle('active');
+  burger.classList.toggle('active');
+});
